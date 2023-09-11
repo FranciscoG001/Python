d4486d72b460e55466ed52a4c248a01d4e3aa093
Gonçalves <francisco-silva.goncalves@capgemini.com>
2023-09-11 14:01:20 +0100

    Add bird collision with ground and random pipes position generation

--- FlappyBird/FlappyBird.py
+++ FlappyBird/FlappyBird.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 # Init setup vars
 width = 640
@@ -7,10 +8,32 @@ height = 720
 running = True
 start = False
 
-velocityPipeMove = 5
-pipePos_x = width
+countPoints = 0
 textColor = (255, 255, 255)
 
+# Init possition
+bird_x = (width/2)-20
+bird_y = height / 2
+
+# Movement
+velBird = 0  
+gravity = 0.5
+impulse = -9
+velocityPipeMove = 5
+pipePos_x = width
+pipePos_x2 = width * 2
+
+# Start coords
+upPipPos_y = -350
+upPipPos_y2 = -250
+downPipPos_y = 500
+downPipPos_y2 = 600
+
+# To random pipes
+upPipPos_y_min = -250
+upPipPos_y_max = -550
+
+# Load Images
 backgroundImage = pygame.image.load("Images/background.jpg")
 upPipeImage = pygame.image.load("Images/upPipe.png")
 downPipeImage = pygame.image.load("Images/downPipe.png")
@@ -18,21 +41,15 @@ flappyBirdTextImage = pygame.image.load("Images/flappyBirdText.png")
 birdImage = pygame.image.load("Images/bird.png")
 playButtonImage = pygame.image.load("Images/playbtn.png")
 
+# Scale original image
 backgroundResizeImage = pygame.transform.scale(backgroundImage, (width, height))
 upPipeResizeImage = pygame.transform.scale(upPipeImage, (width/7, height))
 downPipeRisezeImage = pygame.transform.scale(downPipeImage, (width/7, height))
+upPipeResizeImage2 = pygame.transform.scale(upPipeImage, (width/7, height))
+downPipeRisezeImage2 = pygame.transform.scale(downPipeImage, (width/7, height))
 flappyBirdTextResizeImage = pygame.transform.scale(flappyBirdTextImage, (width, 200))
 birdResizeImage = pygame.transform.scale(birdImage, (75, 75))
 playButtonResizeImage = pygame.transform.scale(playButtonImage, (150, 75))
-
-# Posição inicial do pássaro
-bird_x = (width/2)-20
-bird_y = height / 2
-
-velBird = 0  
-gravity = 0.5
-impulse = -9
-countPoints = 0
 
 # Pygame setup
 pygame.init()
@@ -45,6 +62,8 @@ displayText = font.render(str(countPoints), True, textColor)
 birdResizeImage = birdResizeImage.convert_alpha()
 upPipeResizeImage = upPipeResizeImage.convert_alpha()
 downPipeRisezeImage = downPipeRisezeImage.convert_alpha()
+upPipeResizeImage2 = upPipeResizeImage2.convert_alpha()
+downPipeRisezeImage2 = downPipeRisezeImage2.convert_alpha()
 
 # Pygame Loop
 while running:
@@ -60,6 +79,7 @@ while running:
                 velBird = 0  
                 gravity = 0.5
                 pipePos_x = width
+                pipePos_x2 = width + 400
                 countPoints = 0
                 displayText = font.render(str(countPoints), True, textColor)
             if event.key == pygame.K_SPACE:
@@ -70,45 +90,70 @@ while running:
     screen.blit(backgroundResizeImage,(0,0))
     screen.blit(flappyBirdTextResizeImage,(0,0))
     screen.blit(birdResizeImage,((width/2)-20,height/2))
-    screen.blit(playButtonResizeImage,((width/2)-55,(height/2)-130))
+    screen.blit(playButtonResizeImage,((width/2)-60,(height/2)-130))
 
     if start:
+        
+        # Pips Move Velocity
         pipePos_x -= velocityPipeMove
+        pipePos_x2 -= velocityPipeMove
 
+        # Bird Move
         velBird += gravity
         bird_y += velBird
 
+        # Screen clean
         screen.fill((0,0,0))
-
+        
+        # Show all imagens
         screen.blit(backgroundResizeImage,(0,0))
-        screen.blit(upPipeResizeImage, (pipePos_x, -350))
-        screen.blit(downPipeRisezeImage, (pipePos_x, 500))
-        screen.blit(displayText,(width/2,height-600))
+        screen.blit(upPipeResizeImage, (pipePos_x, upPipPos_y))
+        screen.blit(downPipeRisezeImage, (pipePos_x, downPipPos_y))
+        screen.blit(upPipeResizeImage2, (pipePos_x2, upPipPos_y2))
+        screen.blit(downPipeRisezeImage2, (pipePos_x2, downPipPos_y2))
+
+        if countPoints < 10:
+            screen.blit(displayText,(width/2,height-600))
+        else:
+            screen.blit(displayText,((width/2)-30,height-600))
         screen.blit(birdResizeImage,(bird_x,bird_y))
         
-        if pipePos_x + upPipeResizeImage.get_width() < 0:
+        # Reset Pip
+        if pipePos_x + upPipeResizeImage.get_width() < -50:
+            upPipPos_y = random.randint(upPipPos_y_max,upPipPos_y_min) # New position
+            downPipPos_y = upPipPos_y + 850
             pipePos_x = width
+        # Reset Pip2
+        if pipePos_x2 + upPipeResizeImage2.get_width() < -50:
+            upPipPos_y2 = random.randint(upPipPos_y_max,upPipPos_y_min) # New position
+            downPipPos_y2 = upPipPos_y2 + 850
+            pipePos_x2 = width
 
+        # Ground Touch
+        if bird_y > 600:
+            start = False
+
+        # Count Pip
         if bird_x > pipePos_x + upPipeResizeImage.get_width() and not passagem_registrada:
             countPoints += 1
             displayText = font.render(str(countPoints), True, textColor)
             passagem_registrada = True
         elif bird_x <= pipePos_x + upPipeResizeImage.get_width():
             passagem_registrada = False
+        # Count Pip2
+        if bird_x > pipePos_x2 + upPipeResizeImage2.get_width() and not passagem_registrada2:
+            countPoints += 1
+            displayText = font.render(str(countPoints), True, textColor)
+            passagem_registrada2 = True
+        elif bird_x <= pipePos_x2 + upPipeResizeImage2.get_width():
+            passagem_registrada2 = False
+
+        # Touch Pipes
+
         
-        # Verifique se o pássaro colidiu com os tubos (tanto tubo de cima quanto tubo de baixo)
-        if (
-            # Colisão com o tubo de cima
-            (bird_x + birdResizeImage.get_width() > pipePos_x and bird_x < pipePos_x + upPipeResizeImage.get_width())
-            and
-            (bird_y < -350 or bird_y + birdResizeImage.get_height() > 600 )
-        ) or (
-            # Colisão com o tubo de baixo
-            (bird_x + birdResizeImage.get_width() > pipePos_x and bird_x < pipePos_x + downPipeRisezeImage.get_width())
-            and
-            (bird_y < -350 + 700 or bird_y + birdResizeImage.get_height() > 500)
-        ):
-            start = False  # Encerre o jogo se houver uma colisão
+
+        
+        
 
     pygame.display.flip() # Update Screen
     clock.tick(60) # FPS to 60
